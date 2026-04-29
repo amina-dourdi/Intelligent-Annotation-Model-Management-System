100acb634bc338c13847bf7927b0a901e9a7dd98
amina dourdi <amina.dourdi@etu.uae.ac.ma>
2026-04-29 20:18:19 +0100

    Initialisation du projet Spring Boot NLP

---
+++ src/test/java/com/ensah/nlp_annotation/NlpAnnotationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensah.nlp_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpAnnotationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
