3121ce9334b2d61365a91a0a525e9fd4fe3dae86
Yousra-khallou <yousra.khallou1@etu.uae.ac.ma>
2026-06-19 18:06:37 +0100

    update project

--- src/test/java/com/NlpAnnotationPlatformApplicationTests.java
+++ src/test/java/com/NlpAnnotationPlatformApplicationTests.java
@@ -1,13 +1,13 @@
 package com;
 
+import com.ensah.NlpAnnotationPlatformApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = NlpAnnotationPlatformApplication.class)
 class NlpAnnotationPlatformApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
